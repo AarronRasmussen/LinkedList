e5ea7e3b9e23a98e9cb95c12902496118d4c49aa
AarronRasmussen <arasmussen96@gmaill.com>
2020-04-14 23:01:42 -0600

    Read documentation. Comprehension remains to be seen.

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -1,42 +1,68 @@
 require 'List'
 
-RSpec.describe List, "#init" do
-    context "Tests class init function." do
-        it "Tests that list object was created with default values." do
-            _list = List.new()
-            expect(_list.head).to eq(nil)
+describe List do 
+    it "is initially empty" do
+        expect(subject.head).to eq(nil)
+    end
+
+    describe "#append" do
+        context "When the list is empty" do
+            before {subject.append(1)}
+
+            it "sets the first element" do
+                expect(subject.head.data).to eq(1)
+            end
+
+            it "does not add a second element" do
+                expect(subject.head.nextNode).to eq(nil)
+            end
+        end
+
+        context "When the list has one element" do
+            before {
+                subject.append(1)
+                subject.append(2)
+            }
+
+            it "sets the second element" do
+                expect(subject.head.nextNode.data).to eq(2)
+            end
+
+            it "does not add a third element" do
+                expect(subject.size).to eq(2)
+            end
         end
     end
-end
 
-RSpec.describe List, "#append_function" do
-    context "Testing append function when head = nil." do
-        it "Should set list.head to _node if list.head is nil." do
-            _list = List.new()
-            _list.append(1)
-            expect(_list.head.data).to eq(1)
-            expect(_list.head.nextNode).to eq(nil)
+    describe "#size" do
+        context "When the list is empty" do
+            it "size returns 0" do
+                expect(subject.size).to eq(0)
+            end
+        end
+    
+        context "When the list has one element" do
+            before{subject.append(83)}
+            it "size returns 1" do
+                expect(subject.size).to eq(1)
+            end
+        end
+    
+        context "When the list has two element" do
+            before{subject.append(83)}
+            it "size returns 1" do
+                expect(subject.size).to eq(1)
+            end
         end
     end
-end
 
-RSpec.describe List, "#size_function_when_empty" do
-    context "Testing size function when list is empty." do
-        it "Returns the size of the list" do
-            _list = List.new()
-            _size = _list.size()
-            expect(_size).to eq(0)
-        end
-    end
-end
-
-RSpec.describe List, "prepend_function" do
-    context "Testing prepend function when head = nil" do
-        it "Should set list.head to _node" do
-            _list = List.new()
-            _list.prepend(1)
-            expect(_list.head.data).to eq(1)
-            expect(_list.head.nextNode).to eq(nil)
+    describe "#prepend" do
+        before {subject.prepend(1)}
+        context "Testing prepend function when head = nil" do
+            it "Should set list.head to _node" do
+                expect(subject.head.data).to eq(1)
+                expect(subject.head.nextNode).to eq(nil)
+            end
         end
     end
 end
--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,30 +1,27 @@
 require 'Node'
 
-RSpec.describe Node, "#init" do
-    context "Tests class init function." do
-        it "Tests node object created node with default values data: nil nextNode: nil." do
-            _node = Node.new()
-            expect(_node.data).to eq(nil)
-            expect(_node.nextNode).to eq(nil)
+describe Node do
+        it "is initially created" do
+            expect(subject.data).to eq(nil)
+            expect(subject.nextNode).to eq(nil)
+        end
+
+    describe "#init" do
+        context "When created with a data value of 1" do
+            subject {Node.new(1,nil)}
+            it "sets the value of data to 1" do
+                expect(subject.data).to eq(1)
+            end
         end
     end
-end
 
-RSpec.describe Node, "#init_data_param" do
-    context "Tests class init function." do
-        it "Tests that Node object is created with correct data paramter: 1." do
-            _node = Node.new(1,nil)
-            expect(_node.data).to eq(1)
-        end
-    end
-end
-
-RSpec.describe Node, "#init_nodePtr_param" do
-    context "Tests class init function." do
-        it "Tests node object is created with correct nextNode pointer." do
-            _node1 = Node.new(1)
-            _node0 = Node.new(0, _node1)
-            expect(_node0.nextNode).not_to eq(nil)
+    describe "#init" do
+        context "When created with a nextNode" do
+            let(:_n) { Node.new(2) }
+            subject {Node.new(0, _n)}
+            it "sets the value of nextNode" do
+                expect(subject.nextNode).to eq(_n)
+            end
         end
     end
 end
